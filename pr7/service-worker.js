552ffab53ace5689dbdf99b3a373bb08aa91c234
anicolao <anicolao@users.noreply.github.com>
2026-08-07 15:51:40 +0000

    deploy: 6178968a8a9dab7e8d864923ff6398b3710bbf1f

--- pr7/service-worker.js
+++ pr7/service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "dosage-app-dosage-pr7-";
-const CACHE_NAME = "dosage-app-dosage-pr7-0.1.0-882abd9";
+const CACHE_NAME = "dosage-app-dosage-pr7-0.1.0-043df7c";
 const APP_SHELL = [
   "./",
   "./assets/KaTeX_AMS-Regular-BQhdFMY1.woff2",
@@ -62,7 +62,7 @@ const APP_SHELL = [
   "./assets/KaTeX_Typewriter-Regular-CO6r4hn1.woff2",
   "./assets/KaTeX_Typewriter-Regular-D3Ib7_Hf.ttf",
   "./assets/index-C8IkmCE3.css",
-  "./assets/index-Dx-JaxCg.js",
+  "./assets/index-WXdxkoMX.js",
   "./icons/apple-touch-icon.png",
   "./icons/dosage-192.png",
   "./icons/dosage-512.png",
